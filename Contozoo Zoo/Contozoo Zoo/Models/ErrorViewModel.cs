af5769e33f9f4d4b28008a5c73abde1799fbbf60
Adam Black <adamblack@microsoft.com>
2021-11-03 11:40:23 +0000

    Project initiated.

---
+++ Contozoo Zoo/Contozoo Zoo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Contozoo_Zoo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
